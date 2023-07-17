2b191cc067da41523f503d21156f49d117a03b7d
Ko-Eunseo <koyk0408@naver.com>
2023-07-17 18:54:01 +0900

    feat: 폰트 세팅, 메타태그 세팅

---
+++ src/app/font.ts
@@ -0,0 +1,13 @@
+import { Gothic_A1, Black_Han_Sans } from "next/font/google";
+
+export const gothic = Gothic_A1({
+  subsets: ["latin"],
+  weight: ["100", "400", "700", "900"],
+  display: "swap",
+});
+
+export const blackGothic = Black_Han_Sans({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
