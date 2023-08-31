cb92ea84a20a834a7a8d4f502964197a0ca385fc
Ko-Eunseo <koyk0408@naver.com>
2023-08-31 23:34:38 +0900

    refactor/modal: 모달 상태 타입 수정

--- src/recoil/atoms/modalState.ts
+++ src/recoil/atoms/modalState.ts
@@ -1,15 +1,16 @@
+import { ContentType } from "@/types/contentType";
 import { atom } from "recoil";
 
 export interface ModalType {
   isOpen: boolean;
   contentId: string;
-  contentTypeId: string;
+  contentTypeId?: ContentType["id"];
 }
 export const modalState = atom<ModalType>({
   key: "modalState",
   default: {
     isOpen: false,
     contentId: "",
-    contentTypeId: "",
+    contentTypeId: undefined,
   },
 });
