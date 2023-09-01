78f5ee8721d1247733a1fd7e402071a7a778568f
Ko-Eunseo <koyk0408@naver.com>
2023-09-01 21:52:02 +0900

    design/layout: 디자인 수정

--- src/components/molecules/button/GeolocationBtn.tsx
+++ src/components/molecules/button/GeolocationBtn.tsx
@@ -1,8 +1,19 @@
 import { BiCurrentLocation } from "react-icons/bi";
 import IconBtn from "../../atoms/buttons/iconBtn/IconBtn";
+import { IconContext } from "react-icons";
 
-const GeolocationBtn = ({ onClick }: { onClick: () => void }) => {
-  return <IconBtn Icon={BiCurrentLocation} onClick={onClick} />;
+const GeolocationBtn = ({
+  onClick,
+  iconSize,
+}: {
+  onClick: () => void;
+  iconSize: string;
+}) => {
+  return (
+    <IconContext.Provider value={{ size: iconSize }}>
+      <IconBtn Icon={BiCurrentLocation} onClick={onClick} />
+    </IconContext.Provider>
+  );
 };
 
 export default GeolocationBtn;
--- src/components/templetes/CustomSection.tsx
+++ src/components/templetes/CustomSection.tsx
@@ -6,14 +6,27 @@ import { Selected } from "@/recoil/atoms/selectState";
 import CardList from "../organisms/card/CardList";
 import { CardWrapper } from "../molecules/card/styledCard";
 import { selectOptionSelector } from "@/recoil/selectors/selectOptionSelector";
+import Title from "../molecules/title/Title";
+import { CenterBox } from "../atoms/styles";
 
 const CustomSection = ({ isSelect }: { isSelect: boolean }) => {
   const [selection] = useRecoilState<Selected[]>(selectOptionSelector);
 
   return (
-    <CardWrapper>
-      <CardList selection={isSelect ? selection : []} />
-    </CardWrapper>
+    <>
+      <CenterBox>
+        {isSelect ? (
+          selection?.map((tag, i) => (
+            <Title key={i} type="main" text={`#${tag.value}`} />
+          ))
+        ) : (
+          <Title type="main" text="내 여행 테마는?" />
+        )}
+      </CenterBox>
+      <CardWrapper>
+        <CardList selection={isSelect ? selection : []} />
+      </CardWrapper>
+    </>
   );
 };
 
